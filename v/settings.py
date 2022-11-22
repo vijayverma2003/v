eed28e43734f4fe2b133bda9709e6bad198261ea
Vijay <vijayvermakvp@gmail.com>
2022-11-22 14:15:41 +0530

    Migrating models

--- v/settings.py
+++ v/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar'
-    'v'
+    'debug_toolbar',
+    'invoicing'
 ]
 
 MIDDLEWARE = [
