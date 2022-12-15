2c370349ffba80c592949c50585538d88e19ba8e
Vijay <vijayvermakvp@gmail.com>
2022-12-15 11:48:17 +0530

    Adding email settings

--- v/settings.py
+++ v/settings.py
@@ -168,3 +168,11 @@ DJOSER = {
         'current_user': 'core.serializers.UserSerializer'
     }
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'from@vinvoicing.com'
