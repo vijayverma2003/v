1aac91ff70d7bee06089faa786e03451425656d4
Vijay <vijayvermakvp@gmail.com>
2022-12-23 12:03:16 +0530

    Adding Homepage

--- v/urls.py
+++ v/urls.py
@@ -23,6 +23,7 @@ admin.site.site_header = 'V Admin Panel'
 admin.site.index_title = 'Admin'
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('invoicing/', include('invoicing.urls')),
     path('auth/', include('djoser.urls')),
